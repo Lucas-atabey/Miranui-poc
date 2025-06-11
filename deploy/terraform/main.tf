c358a6a933dbcd7af3c79b1b1e3074c60f72e89a
Lucas <lucas.atabey94510@gmail.com>
2025-06-11 09:29:41 +0200

    FIX terraform init backend-config

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,20 +1,18 @@
 terraform {
+}
+
+terraform {
   required_providers {
     ionoscloud = {
       source  = "ionos-cloud/ionoscloud"
       version = ">= 6.4.10"
     }
   }
-}
-
-terraform {
   backend "s3" {
     bucket                      = "lucas-terraform"
     key                         = "state/terraform.tfstate"
     region                      = "us-east-1" # Random us region not used for ovh backend
     endpoints                   = { s3 = "https://s3.rbx.io.cloud.ovh.net" }
-    access_key                  = var.access_key
-    secret_key                  = var.secret_key
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     use_path_style              = true
